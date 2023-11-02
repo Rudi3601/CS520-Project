875a0f584aa97d99165efb5f95b19bddd0404ed3
Darsh Gondalia <51378290+DarshGondalia@users.noreply.github.com>
2023-11-02 17:35:27 -0400

    testing other branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
       <div className="App">
           <header className="App-header">
-              <h1>React and flask</h1>
+              <h1>Cookie Collector init build</h1>
               {/* Calling a data from setdata for showing */}
               <p>{data.name}</p>
               <p>{data.age}</p>

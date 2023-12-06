610877142be260820d8eaafff0fad8915b236ccc
Prasad-adhi <prasad.adhi08@gmail.com>
2023-12-06 18:48:25 -0500

    Doctor login and schedule pipeline

--- backend-Flask/venv/app.py
+++ backend-Flask/venv/app.py
@@ -1,14 +1,24 @@
 from flask import Flask
-from flask import Flask, render_template, request,jsonify,redirect, url_for
+from flask import Flask, render_template, request,jsonify,redirect, url_for,session
 from pymongo import MongoClient
 import datetime
 from mongo import *
+from urllib.parse import unquote
+from flask_session import Session
  
 x = datetime.datetime.now()
- 
-# Initializing flask app
+
+import secrets
+
+def generate_random_key(length=24):
+    # Generate a random key with the specified length
+    random_key = secrets.token_hex(length // 2)
+    return random_key
+
 app = Flask(__name__, static_url_path='/static')
- 
+app.config["SESSION_PERMANENT"] = False
+app.secret_key = secrets.token_hex(24)
+#Session(app)
 #DB connection
 client = MongoClient("mongodb+srv://super:xCtC5CJfyAWJyvCK@atlascluster.j0uq5ga.mongodb.net/")
  
@@ -56,10 +66,14 @@ def doctor_login_submit():
         username=request.form['username']
         pwd = request.form['password']
         status = doctor_login(username,pwd)
-        print(username,pwd)
-        print(status)
+        #print(username,pwd)
+        #print(status)      
         if(status):
-            return """<html>Welcome Doctor</html>"""
+            session["doctor"] = status["DoctorID"]
+            doctor_info = status["DoctorID"]
+            print(f"Status:{doctor_info}")
+            return redirect(url_for('doctor_portal',doctor_info=doctor_info))
+            #return redirect(url_for('doctor_portal'))
         else:
             return """<html>Wrong username or password</html>"""
 
@@ -112,7 +126,37 @@ def new_patient_submit():
             return redirect(url_for('patient_register_userexist'))
 
 
- 
+@app.route("/doctor_portal",methods=["GET","POST"])
+def doctor_portal():
+    doctor_info = request.args.get('doctor_info')
+    #doctor_info = session.get("doctor")
+    # Convert the string representation of the dictionary back to a dictionary
+    #doctor_info = eval(doctor_info)
+    schedule = get_doctor_schedule(int(doctor_info))
+    #print(doctor_info,schedule)
+    return render_template("doctor-dashboard.html",doctor_info=schedule) 
+
+@app.route("/doctor_appt_view/<appt>")
+def doctor_appt_view(appt):
+    decoded_string = unquote(appt)
+    Day,time = decoded_string.split(" ")
+    #print(Day,time)
+    #schedule = get_doctor_schedule(id)
+    print(session.get("doctor"))
+    schedule = get_doctor_schedule(session.get("doctor"))
+    user_details = get_patient(schedule["Schedule"][Day][time])
+    del user_details["_id"]
+    del user_details["password"]
+    del user_details["hash_key"]
+    #user_details["_id"] = str(user_details["_id"])
+    for key in user_details:
+        print(f"{key}:{type(user_details[key])}")
+    return render_template("doctor-appt-view.html",user_details = user_details)
+
+@app.route("/doctor_logout",methods=["GET","POST"])
+def doctor_logout():
+    session.pop('doctor', None)
+    return redirect(url_for('welcome_page')) 
      
 # Running app
 if __name__ == '__main__':

861f4c9ea08410af18eb4d8fa3be8699b56e38ee
yining0713 <zhuy0713@gmail.com>
2023-12-15 00:33:41 -0500

    add comments for server

--- src/app.py
+++ src/app.py
@@ -43,27 +43,33 @@ Return type true or false:
 True: New user added successfully
 False: Username already exists
 """
-
+#Welcome page route
 @app.route("/")
 def welcome_page():
     return render_template("welcome.html")
 
+#Patient login route
 @app.route("/patient_login")
 def patient_login_page():
     return render_template("patient-login.html")
 
+#Patient register route
 @app.route("/patient_register")
 def patient_register():
     return render_template("patient-register.html")
 
+#Doctor login route
 @app.route("/doctor_login")
 def doctor_login_page():
     return render_template("doctor-login.html")
 
+#Doctor login unauthenticated
 @app.route("/doctor_login_error")
 def doctor_login_error():
     return render_template("doctor-login-error.html")
 
+#Doctor login submit
+# Called when doctor enters login info if username and password are correct, triggers the doctor portal else triggers the error login
 @app.route("/doctor_login_submit",methods=["GET","POST"])
 def doctor_login_submit():
     if request.method =="POST":
@@ -82,10 +88,13 @@ def doctor_login_submit():
         else:
             return redirect(url_for('doctor_login_error'))
 
+#Patient login unauthenticated
 @app.route("/patient_login_error")
 def patient_login_error():
     return render_template("patient_login_error.html")
 
+#Patient login submit
+# Called when patient enters login info if username and password are correct, triggers the doctor portal else triggers the error login
 @app.route("/patient_login_submit",methods=["GET","POST"])
 def patient_login_submit():
     if request.method =="POST":
@@ -104,6 +113,8 @@ def patient_login_submit():
         else:
             return redirect(url_for('patient_login_error'))
 
+
+#Patient portal display, creates sessions
 @app.route("/PatientPortal",methods=["GET","POST"])
 def patient_portal():
     user_details = request.args.get("user_details")
@@ -111,7 +122,7 @@ def patient_portal():
     session["patient"] = user_details["username"]
     return render_template("PatientPortal.html",user_details=user_details)
 
-
+# Checks if username exists while a new patient tries to login
 @app.route("/patient_register_userexist")
 def patient_register_userexist():
     return render_template("patient-register-userexist.html")
@@ -147,16 +158,19 @@ def new_patient_submit():
         else:
             return redirect(url_for('patient_register_userexist'))
 
+#Returns the schedule of the doctor for the patient to book an appointment
 @app.route("/patient-doctor-dashboard/<doctor_id>")
 def patient_doctor_dashboard(doctor_id):
     session["d_id"] = int(doctor_id)
     schedule = get_doctor_schedule(int(doctor_id))
     return render_template("patient-doctor-dashboard.html",doctor_info=schedule)
 
+# Returns a list of doctors present
 @app.route("/DoctorList")
 def doctor_list():
     return render_template("DoctorList.html")
 
+#Display the active schedule of the doctor to  check the list of patients who have booked an appointment
 @app.route("/doctor_portal",methods=["GET","POST"])
 def doctor_portal():
     doctor_info = request.args.get('doctor_info')
@@ -168,6 +182,7 @@ def doctor_portal():
     #print(doctor_info,schedule)
     return render_template("doctor-dashboard.html",doctor_info=schedule) 
 
+# Books an appointment for the patient, receives the doctor,day and time
 @app.route("/patient_appt/<appt>")
 def patient_appt(appt):
     decoded_string = unquote(appt)
@@ -191,6 +206,7 @@ def patient_appt(appt):
     else:
         return """Error in code: Not able to book appointment"""
 
+# Displays the form for the doctor to enter the details about the patient during the interview
 @app.route("/doctor_appt_view/<appt>")
 def doctor_appt_view(appt):
     #print("session[doctor] at doctor_appt_view: " + str(session["doctor"]))
@@ -211,12 +227,14 @@ def doctor_appt_view(appt):
     for key in user_details:
         print(f"{key}:{type(user_details[key])}")
     return render_template("doctor-appt-view.html",user_details = user_details)
-
+ 
+# Doctor logout
 @app.route("/doctor_logout",methods=["GET","POST"])
 def doctor_logout():
     session.pop('doctor', None)
     return redirect(url_for('welcome_page')) 
 
+# Once the doctor hits submit on an appointment all the information entered is received and stored in the database
 @app.route("/appt_submit/<day_time>",methods=["GET","POST"])
 def appt_submit(day_time):
     bpUpper = request.form['bloodPressureUpper']
@@ -240,6 +258,7 @@ def appt_submit(day_time):
     else:
         return """Error"""
 
+#Creates a visualization to the doctor on the previous appointments of the patient
 @app.route("/medical_history_record/<appt_pat_id>",methods=["GET","POST"])
 def medical_history(appt_pat_id):
     decoded_string = unquote(appt_pat_id)
@@ -256,6 +275,7 @@ def medical_history(appt_pat_id):
             appointments_list.append(x)
         return render_template("medical_history_record_v2.html", day=day, time=time, appts=appointments_list)
 
+#patient logout
 @app.route("/patient_logout",methods=["GET","POST"])
 def patient_logout():
     session.pop('patient', None)
